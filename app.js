e9d0bbfe091cc1cbb743d394f60b0796ee37403c
Rannier Reis <rannierreis@gmail.com>
2022-07-05 00:48:07 -0300

    add require eventEmitter

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require ('express')
+const bodyParser = require('body-parser')
+
+const app = express();
+var port = process.env.PORT || 3000;
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended:true}))
+
+
+
+app.get("/", (request,response) =>{
+    response.setHeader('Content-Type','application/json')
+    console.log("Bot Atendimento MS - online")
+    return response.send("Bot Atendimento MS - online")
+
+})
+
+app.post("/", (request,response) => {
+    if(request.headers['Content-Type'] === 'application/json') return response.status(401).json({
+        error: 'Invalid Type',
+        message:'Content-Type must be application/json'
+    })
+    console.log(request.body)
+})
+
+app.listen(port, () => console.log(`Bot sendo executado na porta ${port}`))
