8f1910fd6aff4dab10f8455bc53e0a421357241d
Rannier Reis <rannierreis@gmail.com>
2022-07-05 00:26:26 -0300

    add require eventEmitter

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require ('express')
 const bodyParser = require('body-parser')
+const { EventEmitter } = require('events')
 
 const app = express();
 var port = process.env.PORT || 3000;
