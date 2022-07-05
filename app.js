f6e649ef6164dfe2fba6c01fbfe84f739df8ee83
Rannier Note Acer <rannierreis@gmail.com>
2022-07-05 14:11:53 -0300

    implements events

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require ('express')
 const bodyParser = require('body-parser')
+const { EventEmitter } = require('events')
+
 
 const app = express();
 var port = process.env.PORT || 3000;
@@ -7,20 +9,51 @@ var port = process.env.PORT || 3000;
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended:true}))
 
+class Stater extends EventEmitter {
+	constructor(props) {
+		super(props)
+		this.state = true
+	}
 
+	setState(newState) {
+		this.state = newState || false
+		this.emit('set', newState)
+	}
 
-app.get("/", (request,response) =>{
+	waitForTrue(newState) {
+		return new Promise(resolve => {
+			let check = () => {
+				if (this.state) {
+					this.off('set', check)
+					resolve()
+				}
+			}
+			this.on('set', check)
+			check()
+		})
+	}
+}
+
+const isOpen = new Stater
+
+app.get("/", async (request,response) =>{
     response.setHeader('Content-Type','application/json')
     console.log("Bot Atendimento MS - online")
+    await isOpen.waitForTrue()
+    isOpen.setState(false)
+    isOpen.setState(true)
     return response.send("Bot Atendimento MS - online")
 
 })
 
-app.post("/", (request,response) => {
+app.post("/", async (request,response) => {
     if(request.headers['Content-Type'] === 'application/json') return response.status(401).json({
         error: 'Invalid Type',
         message:'Content-Type must be application/json'
     })
+    await isOpen.waitForTrue()
+    isOpen.setState(false)
+    isOpen.setState(true)
     console.log(request.body)
 })
 
