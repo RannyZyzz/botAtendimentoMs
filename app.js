ed1b3afa7689e15e8f6395d3ed7ce3bdfeed1971
Rannier Reis <rannierreis@gmail.com>
2022-07-04 23:33:09 -0300

    add body-parser to print request body in console

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 const express = require ('express')
+const bodyParser = require('body-parser')
 
 const app = express();
 var port = process.env.PORT || 3000;
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended:true}))
 
 app.get("/", (request,response) =>{
     response.setHeader('Content-Type','application/json')
