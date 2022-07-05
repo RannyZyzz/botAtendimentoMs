00e0bfca996f7e4edd17db35aa0d5e44fcbd9cec
Rannier Reis <rannierreis@gmail.com>
2022-07-05 00:22:47 -0300

    remove eventEmitter

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const express = require ('express')
 const bodyParser = require('body-parser')
-const { EventEmitter } = require('events')
 
 const app = express();
 var port = process.env.PORT || 3000;
@@ -8,38 +7,9 @@ var port = process.env.PORT || 3000;
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended:true}))
 
-class Starter extends EventEmitter {
-    constructor(props) {
-        super(props)
-        this.state = true
-    }
-
-    setState(newState){
-        this.state = newState || false
-        this.emit('set', newState)
-    }
-
-    waitForTrue(newState){
-        return new Promise(resolve =>{
-            let check = () => {
-                if(this.state){
-                    this.off('set',check)
-                    resolve()
-                }
-            }
-            this.on('set',check)
-            check()
-        })
-    }
-}
-
-const isState = new Starter
-
 app.get("/", (request,response) =>{
     response.setHeader('Content-Type','application/json')
     console.log("Bot Atendimento MS - online")
-    isState.waitForTrue()
-    isState.setState(true)
     return response.send("Bot Atendimento MS - online")
 
 })
@@ -50,8 +20,6 @@ app.post("/", (request,response) => {
         message:'Content-Type must be application/json'
     })
     console.log(request.body)
-    isState.waitForTrue()
-    isState.setState(true)
 })
 
 app.listen(port, () => console.log(`Bot sendo executado na porta ${port}`))
