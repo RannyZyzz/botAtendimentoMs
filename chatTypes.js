fe6974428413d5c97599ff71548ea6ab39bfdc6d
Rannier Reis <rannierreis@gmail.com>
2022-07-06 00:38:03 -0300

    add chatTypes.js function botOption2

--- chatTypes.js
+++ chatTypes.js
@@ -1,7 +1,7 @@
 import { sendBotMessage } from './mosiaCaller.js'
 
 export async function typeAttend(chatProtocol){
-    const chatMenuOptions = 'Escolha uma das opções:\n1- Abrir chamado;\n2- Consultar chamado aberto;\n3- Consultar documentação técnica;\n4- Encerrar Atendimento'
+    const chatMenuOptions = 'Escolha uma das opções:\n1- Abrir chamado;\n2- Falar com um de nossos atendentes;\n3- Consultar status online;\n4- Encerrar Atendimento'
     try{
       await sendBotMessage(chatProtocol,"message",chatMenuOptions)  
     }
@@ -14,6 +14,12 @@ export async function botOption1(chatProtocol){
     await sendBotMessage(chatProtocol,'message','ATENÇÃO!\nEstamos implementando a funcionalidade para abrir chamados diretamente pelo MosiaChat'+
     '\n\nAté lá, acesse este link e abra seu ticket.\nhttps://mobilesaude.zendesk.com/hc/pt-br/requests/new'+
     '\n\nDeseja encerrar este atendimento?\n0-Retornar ao Menu\n4-Encerrar atendimento')
+}
+
+export async function botOption2(chatProtocol){
+  await sendBotMessage(chatProtocol,chatType,'Só um momento iremos direcionar para um de nossos Atendentes').then(
+    await sendBotMessage(chatProtocol,"transfer")
+  )
 }
 
 export async function botOption4(chatProtocol){
