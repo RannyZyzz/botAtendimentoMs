306d8f8e3888ae35508db2ce4da241f083ea6799
Rannier Reis <rannierreis@gmail.com>
2022-07-05 22:07:36 -0300

    adjust post method

--- app.js
+++ app.js
@@ -1,40 +1,11 @@
 const express = require ('express')
 const bodyParser = require('body-parser')
-const { EventEmitter } = require('events')
-
 
 const app = express();
 var port = process.env.PORT || 3000;
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended:true}))
-
-class Stater extends EventEmitter {
-	constructor(props) {
-		super(props)
-		this.state = true
-	}
-
-	setState(newState) {
-		this.state = newState || false
-		this.emit('set', newState)
-	}
-
-	waitForTrue(newState) {
-		return new Promise(resolve => {
-			let check = () => {
-				if (this.state) {
-					this.off('set', check)
-					resolve()
-				}
-			}
-			this.on('set', check)
-			check()
-		})
-	}
-}
-
-const isOpen = new Stater
 
 app.get("/", (request,response) =>{
     response.setHeader('Content-Type','application/json')
@@ -50,14 +21,8 @@ app.post("/", async (request,response) => {
     })
     
     console.log(request.body)
-    const chatProtocol = request.body.protocol 
-    const chatType = request.body.type 
-    const chatMessage = request.body.message 
     
-    
-    await isOpen.waitForTrue()
-    isOpen.setState(false)
-    isOpen.setState(true)
+    return response.sendStatus(200)
 })
 
 app.listen(port, () => console.log(`Bot sendo executado na porta ${port}`))
