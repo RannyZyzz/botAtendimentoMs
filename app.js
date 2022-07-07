63a3e9a83776942dbbbc6a0a20e5f648e474df48
Rannier Note Acer <rannierreis@gmail.com>
2022-07-07 16:57:10 -0300

    adjust logic menu bar

--- app.js
+++ app.js
@@ -59,11 +59,11 @@ app.post("/", async (request,response) => {
             await botOption4(chatProtocol)
         }
         else if(chatMessage != '0' || chatMessage != '1' || chatMessage != '2' || chatMessage != '3' || chatMessage != '4'){
-                async () =>{  
+                (async () =>{  
                     var result = await botWrongOption(chatProtocol)
                     result = await typeAttend(chatProtocol)
-                    return result
-                }
+                    return result;
+                })()
         }
     }
     
