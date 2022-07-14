1f5f2bf0fc60de2f6b17f02908d11d7cb37179a2
Rannier Note Acer <rannierreis@gmail.com>
2022-07-14 15:39:06 -0300

    Create integration Zendesk

--- axios.js
+++ axios.js
@@ -57,7 +57,7 @@ export async function ticketZendesk(idTicket,emailAuthentication){
         array.push('Atribuído: ' + objAssingnee.data.user.name)
     
         ticketDataEntrega.map((id) =>{
-            if(id.id == '7707139955859'){
+            if(id.id == '7707139955859' && id.value != ''){
                 var dateDelivery = formatDate(id.value)
                 console.log("Previsão de entrega: " + dateDelivery)
                 array.push("Previsão de entrega: " + dateDelivery)
